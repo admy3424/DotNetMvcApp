0007805aa632f73fd4598ffab2dd857c8548ce96
Maya <addisumaya34@gmail.com>
2025-01-01 18:40:49 -0500

    Removed inf scroll and added gis project 1 imgs

--- Models/Post.cs
+++
@@ -1,7 +0,0 @@
-// Define structure of data
-public class Post
-{
-    public int Id { get; set; }
-    public string Title { get; set; } = string.Empty; 
-    public string Content { get; set; } = string.Empty; 
-}
