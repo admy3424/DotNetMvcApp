b54921d94231105a62eee82f2fdf3cb6a0d3e9ac
Maya <addisumaya34@gmail.com>
2025-01-02 15:01:38 -0500

    Cleared content & created least deployable product

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,10 +1,6 @@
 // MVC Framework features
 using Microsoft.AspNetCore.Mvc;
 
-using System.IO;
-using System.Linq;
-using System.Collections.Generic;
-
 // Define Controller
 public class HomeController : Controller
 {   
@@ -13,22 +9,6 @@ public class HomeController : Controller
     {
         // Set page title
         ViewData["Title"] = "Home";
-
-
-        // Get image paths from the folder
-        string folderPath = Path.Combine(Directory.GetCurrentDirectory(), "wwwroot/images/gis_project_1");
-        List<string> imagePaths = new List<string>();
-
-         // Check if the directory exists before attempting to fetch images
-        if (Directory.Exists(folderPath))
-        {
-            string[] files = Directory.GetFiles(folderPath);
-
-            // Get relative paths for rendering in the view
-            imagePaths = files.Select(file => "/images/gis_project_1/" + Path.GetFileName(file)).ToList();
-        }
-
-        // Pass image paths to the view
-        return View(imagePaths);
+        return View();
     }
 }
